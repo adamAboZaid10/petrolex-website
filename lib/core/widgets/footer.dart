7f1db686c6c0714f1ecfc55a2682b2b019669253
Zahraa-Salem <zahraasalem144@gmail.com>
2024-07-02 04:02:34 +0300

    "added some filler code to footer"

---
+++ lib/core/widgets/footer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FooterWidget extends StatelessWidget {
+  const FooterWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Row(
+        children: [
+
+      ],),
+    );
+  }
+}
